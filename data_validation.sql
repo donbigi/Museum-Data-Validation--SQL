3fa2472d2a097e43af2ed4bc522a8c94877929d0
donbigi <ucosibe@gmail.com>
2022-01-04 19:21:53 +0100

    Updated Solution

--- data_validation.sql
+++ data_validation.sql
@@ -1,3 +1,30 @@
+-- Museum Exhibition Data Validation
+-- Uploading CSV file to PostgreeSQL
+
+DROP TABLE IF EXISTS "emperors";
+
+-- Creating table to store CSV file
+CREATE TABLE emperors (
+index VARCHAR(50) PRIMARY KEY,
+name VARCHAR(50),
+full_name VARCHAR,
+birth VARCHAR(50),
+death VARCHAR(50),
+birth_city VARCHAR(50),
+birth_province VARCHAR(50),
+succession VARCHAR(50),
+reign_start VARCHAR(50),
+reign_end VARCHAR(50),
+cause VARCHAR(50),
+killer VARCHAR(50),
+dynasty VARCHAR(50),
+era VARCHAR(50)
+);
+
+-- Copying CSV file to PostgreSQL
+COPY emperors FROM 'roman_emperors.csv' DELIMITER ',' CSV HEADER;
+
+-- Solution
 SELECT DISTINCT(UPPER(name)) AS name, -- Makes name uppercase
 		LOWER(full_name) AS full_name, -- Makes full_name lowercase
 		CASE WHEN birth_city IS NULL AND birth_province IS NULL THEN 'Unknown'
